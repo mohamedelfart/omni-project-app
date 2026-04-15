6afc7652ea31dc657fb596f7941bc11574fb75d9
MohamedElfart <Mohamedaboalazm35@gmail.com>
2026-04-15 04:57:18 +0300

    test: update widget test to accommodate delayed mock loading in ServiceManager

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@ import 'package:omnirent_app/main.dart';
 void main() {
   testWidgets('OmniRentApp builds', (WidgetTester tester) async {
     await tester.pumpWidget(const OmniRentApp());
-    await tester.pump();
+    // Property list uses a 1s delayed mock load in ServiceManager.searchProperties.
+    await tester.pump(const Duration(milliseconds: 1100));
   });
 }
